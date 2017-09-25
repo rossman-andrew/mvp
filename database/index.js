49545350d848c6bceb01d5e279dd3ddae583169c
Andrew Rossman <andrew100r@gmail.com>
2017-09-25 16:04:56 -0700

    set up mongo database and schema

--- database/index.js
+++ database/index.js
@@ -11,12 +11,27 @@ db.on('error', function() {
 db.once('open', function() {
 	console.log('Database connection successful!');
 	// Code to be executed goes here, in this callback.
-	var doTime = mongoose.Schema({
-		toDo: String,
-		predictedTime: Number,
-		actualTime: Number
-	});
 
-	var doTimeItem = mongoose.model('doTimeItem', doTime);
-	
 });
+var doTime = mongoose.Schema({
+	toDo: String,
+	predictedTime: Number,
+	actualTime: Number
+});
+
+var doTimeItem = mongoose.model('doTimeItem', doTime);
+
+var selectAll = function(callback) {
+	doTimeItem.find(function(err, items) {
+		if (err) {
+			callback(err, null);
+		} else {
+			callback(null, items);
+		}
+	});
+}
+
+module.exports.selectAll = selectAll; 
+
+
+
