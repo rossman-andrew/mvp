6f34d38238adbc3186d4d1e22f1aabc3b2f159c1
Andrew Rossman <andrew100r@gmail.com>
2017-09-25 23:09:23 -0700

    implemented front end version of start and stop times as well as duration for to do list items

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -7,45 +7,48 @@ class Task extends React.Component {
 		this.state = {
 			clickNum: 0,
 			startTime: 0,
-			stopTime: 0
+			stopTime: 0,
+			actualTime: 0
 		}
 	}
 
 	setStartTime() {
-		let startTimeObj = {
-			startTime: Date.now(),
-			toDo: this.props.individualTaskProp.toDo
-		};
 		if (this.state.clickNum === 0) {
+			let startTimeObj = {
+				startTime: new Date().toLocaleString(),
+				toDo: this.props.individualTaskProp.toDo
+			};
 			this.setState({
 				clickNum: this.state.clickNum + 1,
-				startTime: 0.1
+				startTime: startTimeObj.startTime
 			});
+			$.ajax({
+	      method: 'POST',
+	      url: '/toDos',
+	      data: JSON.stringify(startTimeObj),
+	      contentType: 'application/json',
+	      success: function(data) {
+	      	console.log("Successful post of start time");
+	      },
+	      error: function(err) {
+	      	console.log("Unsuccessful post of start time");
+	      }
+	    });
 		} else if (this.state.clickNum === 1) {
 			this.setState({
 				clickNum: this.state.clickNum + 1,
-				stopTime: Date.now()
-			})
+				stopTime: new Date().toLocaleString(),
+				actualTime: (Date.parse(new Date().toLocaleString()) - Date.parse(this.state.startTime)) / 60000
+			});
 		}
-		$.ajax({
-      method: 'POST',
-      url: '/toDos',
-      data: JSON.stringify(startTimeObj),
-      contentType: 'application/json',
-      success: function(data) {
-      	console.log("Successful post of start time");
-      },
-      error: function(err) {
-      	console.log("Unsuccessful post of start time");
-      }
-    });
 	}
 
 	render() {
 		return (
 			<div onClick={this.setStartTime.bind(this)}>
 				<h4> Do Time Item: {this.props.individualTaskProp.toDo} </h4>
-				<p>Predicted Time: {this.props.individualTaskProp.predictedTime} Start Time: {this.state.startTime} Stop Time: {this.state.stopTime} Actual Time: {this.props.individualTaskProp.actualTime}</p>
+				<p>Predicted Time: {this.props.individualTaskProp.predictedTime} Actual Time (minutes): {this.state.actualTime}</p>
+				<p>Start Time: {this.state.startTime} Stop Time: {this.state.stopTime}</p>
 				<br />
 			</div>
 		)
